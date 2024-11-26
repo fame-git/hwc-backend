22d1391c4d6888b8e6916494a0b5aa7edd24f5a8
Ratchanont Treevijitpaisan <r84389141@china.huawei.com>
2024-11-26 16:27:34 +0700

    geppety #3

--- master-acc/backend.tf
+++ master-acc/backend.tf
@@ -1,19 +1,19 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucket"
-    key    = "master/terraform.tfstate"
-    region = "ap-southeast-2"
-    endpoints = {
-      s3 = "https://obs.ap-southeast-2.myhuaweicloud.com"
-    }
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-state-bucket"
+#     key    = "master/terraform.tfstate"
+#     region = "ap-southeast-2"
+#     endpoints = {
+#       s3 = "https://obs.ap-southeast-2.myhuaweicloud.com"
+#     }
 
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-  }
-}
+#     skip_region_validation      = true
+#     skip_credentials_validation = true
+#     skip_metadata_api_check     = true
+#     skip_requesting_account_id  = true
+#     skip_s3_checksum            = true
+#   }
+# }
 
 # terraform {
 #   backend "local" {
